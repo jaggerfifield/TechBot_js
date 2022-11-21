4a7939f72ba66eb96b725a555ef368dc7d755de9
Jagger Fifield <setUnlocalizedName@gmail.com>
2022-11-21 16:31:20 -0700

    Cleanup info

--- info.js
+++ info.js
@@ -4,28 +4,28 @@ module.exports = {
 	call: call
 };
 
-var score = require('./score.js');
+let score = require('./score.js');
 
-const { MessageEmbed } = require('discord.js')
+// const { MessageEmbed } = require('discord.js')
 
 function call(interaction){
 	// The info command has sub-commands, get them here.
-	var type = interaction.options.getSubcommand(false);
-	var string = 'WIP';
+	let type = interaction.options.getSubcommand(false);
+	let string = 'WIP';
 	
-	if(type == 'user'){
-		var user = interaction.options.getUser('username');
+	if(type === 'user'){
+		let user = interaction.options.getUser('username');
 		
 		if(user == null){
 			user = interaction.user;
 		}
 
-		var sent = score.get(interaction.guildId, user.id);
+		let sent = score.get(interaction.guildId, user.id);
 
 		string = `\`\`\`Username: ${user.username}#${user.discriminator}\nID: ${user.id}\nIs Bot: ${user.bot}\nMessages sent: ${sent}\`\`\``
 		
-	}else if(type == 'server'){
-		var server = interaction.member.guild;
+	}else if(type === 'server'){
+		let server = interaction.member.guild;
 		string = `\`\`\`Name: ${server.name}\nID: ${server.id}\nMembers: ${server.memberCount}\`\`\``;
 	}
 	
